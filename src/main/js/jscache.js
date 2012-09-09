81fca624f53bf933bfa4447fa7d9be0d01cda177
Olly Oechsle <owksley@gmail.com>
2012-09-09 21:56:22 +0100

    Added basic non-expiring cache functionality

--- src/main/js/jscache.js
+++ src/main/js/jscache.js
@@ -1,9 +1,51 @@
 (function() {
-    
-    function JsCache() {
+
+    if (!Object.create) {
+        Object.create = function (o) {
+            function F() {
+            }
+
+            F.prototype = o;
+            return new F();
+        };
+    }
+
+    function cache(fn) {
+
+        return function() {
+
+            var cached = fn["__cached"];
+
+            if (!cached) {
+                cached = fn.apply(this, arguments);
+                fn["__cached"] = cached;
+            }
+
+            return cached;
+
+        };
 
     }
 
-    window.JsCache = JsCache;
+    function init(proxy, object) {
+
+        var key, fn;
+
+        for (key in object) {
+
+            fn = object[key];
+
+            if (typeof fn === "function") {
+                proxy[key] = cache(fn);
+            }
+
+        }
+    }
+
+    Object.cache = function (object) {
+        var proxy = Object.create(object);
+        init(proxy, object);
+        return proxy;
+    };
 
 })();
--- src/test/js/jscacheTest.js
+++ src/test/js/jscacheTest.js
@@ -1,17 +1,76 @@
 (function() {
 
+    var dao;
+
     module("jscache", {
         setup: function() {
-
+            dao = {
+                value: 0,
+                getValue: function() {
+                    return this.value;
+                }
+            };
         },
         teardown: function() {
 
         }
     });
 
-    test("test1", function() {
+    test("Object.cache exists", function() {
 
-        ok(window.JsCache);
+        ok(Object.cache);
+        equal(typeof Object.cache, "function");
+
+    });
+
+    test("functions are wrapped", function() {
+
+        var cached = Object.cache(dao);
+
+        ok(cached, "There is a cached object");
+
+        ok(cached.getValue, "Has wrapped around getValue");
+
+    });
+
+    test("functions still return expected values", function() {
+
+        var cached = Object.cache(dao);
+
+        dao.value = 10;
+
+        equal(cached.getValue(), 10, "Method still returning the expected value");
+
+    });
+
+    test("value is cached on the second call", function() {
+
+        var cached = Object.cache(dao);
+
+        dao.value = 10;
+
+        equal(cached.getValue(), 10, "Underlying method is called the first time");
+
+        dao.value = 20;
+
+        equal(cached.getValue(), 10, "Cache is used the second time");
+
+    });
+
+    test("variable are wrapped", function() {
+
+        var obj = {
+            counter: 1
+        };
+
+        var cached = Object.cache(obj);
+
+        equal(cached.counter, 1, "The value on the object should be available on the cached object");
+
+        obj.counter++;
+
+        equal(cached.counter, 2, "Changes to the counter on the sub object should be accessible on the cached object");
+
 
     });
 
